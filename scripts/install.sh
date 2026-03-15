4381181913f0c78dbfee36124e33b3eaba06bea2
Gabriel Hare <ghare@reification.io>
2026-03-15 12:59:48 -0700

    Fix: run_dino_sam2 works

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,8 @@
 conda create -n visualsync python=3.10
 conda activate visualsync
-pip install --no-cache-dir torch==2.5.1 torchvision==0.20.1 torchaudio==2.5.1 --index-url https://download.pytorch.org/whl/cu124
+pip install --no-cache-dir torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu130
+
+pip install --upgrade setuptools
 
 cd Tracking-Anything-with-DEVA
 pip install -e .
@@ -11,3 +13,4 @@ pip install -e .
 pip install --no-build-isolation -e grounding_dino
 pip install openai imageio dotenv transformers einops imgcat
 pip install imageio[ffmpeg]
+cd ..
